f6db2f167c541a57245ab991fe6b12c140770198
DartZaiac <33229603+DartZaiac@users.noreply.github.com>
2025-04-03 23:10:42 +0300

    Pre final. 4 players complite

--- main.py
+++ main.py
@@ -6,67 +6,66 @@ from PIL import ImageTk, Image
 
 class Example:
     def __init__(self):
-
+        # Инициализация кнопки с помощью которой будем работать. Имено здесь будет храниться инфа, кому добавлять карты и какую удалять
         self.curButt = ""
 
+        # Списки карт каждого игрока
         self.listRed=[]
         self.listBlue = []
         self.listYellow = []
         self.listGreen = []
 
+        # Список отключённых карт из империума
         self.listOfDisabled = []
 
+        # Загружаем список колод
         file = open('AddList.json')
         listOfAddons = json.load(file)
         file.close()
         
-        directory = ".\Images"
-        # Создаем пустой список
-        files = []
 
-        # Добавляем файлы в список
-        files += os.listdir(directory)
-        # print(files)
+        # directory = ".\Images"
+        # # Создаем пустой список
+        # files = []
+
+        # # Добавляем файлы в список
+        # files += os.listdir(directory)
 
 
-        self.columsPlayers = 3
+        # self.columsPlayers = 3
 
-        wid = 4 * self.columsPlayers * 25
-        print(wid)
-        wid4 = wid//2
-        print(wid4)
+        # wid = 4 * self.columsPlayers * 25
+
+        # Создаём окно
         self.root = tk.Tk()
         self.root.title("Dune Imperium Блокнот")
 
         # menu left
-        self.menu_left = tk.Frame(self.root, width= wid, bg="#f0f0f0")
+        self.menu_left = tk.Frame(self.root, bg="#f0f0f0")
+        # Поле для режима игроков (4 или 6)
         self.menu_left_upper = tk.Frame(self.menu_left,   bg="red",)
-        self.menu_left_color = tk.Frame(self.menu_left,  bg="blue")
-        self.menu_left_cards = tk.Frame(self.menu_left,  bg="orange")
+        # Поле для 4 полей колод игроков
+        self.menu_left_color = tk.Frame(self.menu_left,  )
+        # self.menu_left_cards = tk.Frame(self.menu_left,  bg="orange")
+        # Поле для кнопки удаления
         self.menu_left_Delete = tk.Frame(self.menu_left,  bg="blue")
-        # self.test = tk.Label(self.menu_left_upper, text="test")
-        # self.test.pack()
-
+        
+        # Размещаем девые поля
         self.menu_left_upper.grid(column=0,row=0)
         self.menu_left_color.grid(column=0,row=1)
-        self.menu_left_cards.grid(column=0,row=2)
-        self.menu_left_Delete.grid(column=0,row=3)
+        self.menu_left_Delete.grid(column=0,row=2)
+
         # self.menu_left_upper.pack(side="top", fill="both", expand=True)
         # self.menu_left_lower.pack(side="top", fill="both", expand=True)
 
+        # Добавляем кружки выбора количества игроков TODO: сделать для 6 игроков
         self.mode  = StringVar(value="play4") 
         self.play4 = tk.Radiobutton(self.menu_left_upper, text="4 игрока",  value="play4", variable=self.mode)
         self.play6 = tk.Radiobutton(self.menu_left_upper, text="6 игроков", value="play6", variable=self.mode)
         self.play4.grid(row = 0, column = 0)
         self.play6.grid(row = 0, column = 1)
 
-        
-
-        
-
-        # Menu Colors
-        
-
+        # Menu Colors. Поля цветов игроков       
         self.RED = tk.Frame(self.menu_left_color, bg="red", borderwidth=2)
         self.RED.grid(column=0,row=1)
 
@@ -79,74 +78,56 @@ class Example:
         self.BLUE = tk.Frame(self.menu_left_color, bg="blue",borderwidth=2)
         self.BLUE.grid(column=3,row=1)
 
-        # self.radioRed = tk.Radiobutton(self.RED,bg="red")
-        # self.radioRed.grid(column=0,row=0,columnspan=2)
-
-        # self.radioGreen = tk.Radiobutton(self.GREEN,bg="green")
-        # self.radioGreen.grid(column=0,row=0,columnspan=2)
-
-        # self.radioYellow = tk.Radiobutton(self.YELLOW,bg="yellow")
-        # self.radioYellow.grid(column=0,row=0,columnspan=2)
-        
-        # self.radioBlue = tk.Radiobutton(self.BLUE,bg="blue")
-        # self.radioBlue.grid(column=0,row=0,columnspan=2)
-        
-        # self.delete_red = tk.Button(self.RED,text = "Delete", command=self.DeleteCard)
-        # self.delete_green = tk.Button(self.GREEN,text = "Delete", command=self.DeleteCard)
-        # self.delete_yellow = tk.Button(self.YELLOW,text = "Delete", command=self.DeleteCard)
+        # Кнопка удаления выбраной кнопки
         self.delete_all = tk.Button(self.menu_left_Delete,text = "Delete", command=self.DeleteCard)
 
-        
-
-        # right area
+        # Правая область вся
         self.right = tk.Frame(self.root, bg="#dfdfdf")
-        self.right_up = tk.Frame(self.right, bg="#dfdfdf")
-        self.right_mid = tk.Frame(self.right, bg="blue")
+        # Строка с аддонами
+        self.right_Addons = tk.Frame(self.right, bg="#dfdfdf")
+        # Область с картами Империума
+        self.righе_Imperium_Cards = tk.Frame(self.right, bg="blue")
+
+        # Область с Сёстрами/Червём/Свёрнутым пространством TODO доделать!
         self.right_down_worm = tk.Frame(self.right, bg="red")
 
-        # self.some_title = tk.Label(self.some_title_frame, text="some title", bg="#dfdfdf")
-        # self.some_title.pack()
+        # Стартуем с Дюны Империи
         self.Addon = StringVar(value="I")
-        print(self.Addon.get())
         shortList=list(listOfAddons.keys())
         self.listOfAddonsRadio = []
         c=0
+        # Добавляем все Аддоны из json
         for key in shortList[4:]:
-            self.listOfAddonsRadio.append(tk.Radiobutton(self.right_up, text=key,  value=listOfAddons[key], variable=self.Addon, command=self.SmenaAddona, anchor="w"))
-            # self.listOfAddonsRadio[c].grid(column = c, row=0)
+            self.listOfAddonsRadio.append(tk.Radiobutton(self.right_Addons, text=key,  value=listOfAddons[key], variable=self.Addon, command=self.SmenaAddona, anchor="w"))
             self.listOfAddonsRadio[c].pack(side = tk.LEFT,anchor="w")
             c+=1
 
-        # self.canvas_area = tk.Canvas(self.root, width=500, height=400, background="#ffffff")
-        # self.canvas_area.grid(row=1, column=1)
-
-        # # status bar
-        self.status_frame = tk.Frame(self.root)
-        self.status = tk.Label(self.status_frame, text="this is the status bar")
-        self.status.pack(fill="both", expand=True)
-
+        # Размещаем панели
         self.menu_left.grid(row=0, column=0, rowspan=2, sticky="nsew")
         self.right.grid(row=0, column=1, sticky="ew")
-        self.right_up.grid(row=0,column=0)
-        
-        self.right_mid.grid(row=1,column=0)
+        self.right_Addons.grid(row=0,column=0)
+        self.righе_Imperium_Cards.grid(row=1,column=0)
         self.right_down_worm.grid(row=2,column=0)
-        # self.canvas_area.grid(row=1, column=1, sticky="nsew") 
-        self.status_frame.grid(row=2, column=0, columnspan=2, sticky="ew")
+        
 
         self.root.grid_rowconfigure(1, weight=1)
         self.root.grid_columnconfigure(1, weight=1)
         self.listOfButtonsButt = []
+
+        # Подготавливаем стартовые карты
         self.StartCards()
+        # Рисуем Империум
         self.SmenaAddona()
 
+        # Запускаем аддон
         self.root.mainloop()
 
     def SmenaAddona(self):
-        # print(self.Addon.get())
+        # Убираем ВСЕ кнопки Империума
         for button in self.listOfButtonsButt:
             button.destroy()
      
+        # Готовим хранилища для размещения
         self.listOfButtons = {
             "1":[],
             "2":[],
@@ -163,17 +144,19 @@ class Example:
         # Добавляем файлы в список
         files += os.listdir(directory)
 
+        # Добавляем все пути до картинок в список картинок по ценам
         for f in files:
             if f[0]==self.Addon.get():
                 self.listOfButtons[str(f[1])].append(str(f))
                 
-        # print(self.listOfButtons)
+        # Список самих кнопок
         self.listOfButtonsButt =[]
         for price in range(1,9):
             col=1
             for but in self.listOfButtons[str(price)]:
                 img_dir = directory + but
                 
+                # Размер картинок
                 w = 35
                 h = 49
 
@@ -183,19 +166,21 @@ class Example:
                 img = Image.open(img_dir)
 
                 img = img.resize((w,h))
-                # img.save(img_dir)
 
                 img = ImageTk.PhotoImage(img)
                 
-                button = tk.Button(self.right_mid, image=img,width=w,height=h,bd=0,text=img_dir)
+                button = tk.Button(self.righе_Imperium_Cards, image=img,width=w,height=h,bd=0,text=img_dir)
                 button.image = img
                 self.listOfButtonsButt.append(button)
+                # Помечаем карты, что отмечены отключением как отключённые
                 if img_dir in self.listOfDisabled:
                     button['state'] = 'disabled'
+                # Размещаем кнопку-карту
                 button.grid(row=price-1,column=col,ipadx=0,ipady=0,padx=0,pady=0)
                 button.bind('<Button-1>',self.AddCard)
                 col+=1
 
+    # Раздача игрокам стартовых карт
     def StartCards(self):
         directory = ".\Images\\S\\"
         files = []
@@ -217,62 +202,90 @@ class Example:
             
             button = tk.Button(self.RED, image=img,width=w,height=h,bd=0,bg='red')
             button.image = img
-            
             button.bind('<Button-1>',self.SelectCardPlayer)
             self.listRed.append(button)
 
             button = tk.Button(self.GREEN, image=img,width=w,height=h,bd=0,bg = 'green')
             button.image = img
-            button.grid(row=r,column=c)
             button.bind('<Button-1>',self.SelectCardPlayer)
+            self.listGreen.append(button)
 
             button = tk.Button(self.YELLOW, image=img,width=w,height=h,bd=0,bg='yellow')
             button.image = img
-            button.grid(row=r,column=c)
             button.bind('<Button-1>',self.SelectCardPlayer)
+            self.listYellow.append(button)
             
             button = tk.Button(master=self.BLUE, image=img,width=w,height=h,bd=0,bg = 'blue')
             button.image = img
-            button.grid(row=r,column=c)
             button.bind('<Button-1>',self.SelectCardPlayer)
+            self.listBlue.append(button)
             
             n+=1
+        # TODO Сделать раздачу 6 игрокам
+        # if
 
-        # self.delete_red.grid   (column=0, row=r+1, columnspan=2)
-        # self.delete_green.grid (column=0, row=r+1, columnspan=2)
-        # self.delete_yellow.grid(column=0, row=r+1, columnspan=2)
+        # отрисовка кнопок-карт игроков
         self.Redraw(self.listRed)
+        self.Redraw(self.listGreen)
+        self.Redraw(self.listYellow)
+        self.Redraw(self.listBlue)
+        # Отрисовка кнопки удаления
         self.delete_all.grid  (column=0, row=0, columnspan=1)
 
+    # Добавление карт из империума
     def AddCard(self,e):
+        # Заранее перерисовываем карту
         w = e.widget
+        wid =25
+        h = 35
+        img = Image.open( w['text'])
+        img = img.resize((wid,h))
+        img = ImageTk.PhotoImage(img)
+        # Если карта-кнопка доступна
         if w["state"] == "normal":
-    # try:  
-            if self.curButt['bg']=='red':
-                w["state"] = "disabled"
-                self.listOfDisabled.append(w["text"])
-                # img = w.image
-                # print(img)
-                wid =25
-                h = 35
-                # img = ImageTk.getimage( imgTk )
-                img = Image.open( w['text'])
-                img = img.resize((wid,h))
-                # img.save("123")
-                img = ImageTk.PhotoImage(img)
-
-                button = tk.Button(self.RED,width=wid,height=h,bd=0,image = img, bg='red')
-                button.image = img
-
-                button.bind('<Button-1>',self.SelectCardPlayer)
+            try:  
+                # Если последняя кнопка карта использованая на игроке - это красная 
+                if self.curButt['bg']=='red':
+                    # Деактивируем кнопку
+                    w["state"] = "disabled"
+                    # Добавляем карту в список неактивных
+                    self.listOfDisabled.append(w["text"])
+                    # Добавляем кнопку 
+                    button = tk.Button(self.RED,width=wid,height=h,bd=0,image = img, bg='red')
+                    button.image = img
+                    button.bind('<Button-1>',self.SelectCardPlayer)
+                    self.listRed.append(button)
+                    # перерисовываем красныую колоду
+                    self.Redraw(self.listRed)
+                elif self.curButt['bg']=='green':
+                    w["state"] = "disabled"
+                    self.listOfDisabled.append(w["text"])
+                    button = tk.Button(self.GREEN,width=wid,height=h,bd=0,image = img, bg='green')
+                    button.image = img
+                    button.bind('<Button-1>',self.SelectCardPlayer)
+                    self.listGreen.append(button)
+                    self.Redraw(self.listGreen)
+                elif self.curButt['bg']=='yellow':
+                    w["state"] = "disabled"
+                    self.listOfDisabled.append(w["text"])
+                    button = tk.Button(self.YELLOW,width=wid,height=h,bd=0,image = img, bg='yellow')
+                    button.image = img
+                    button.bind('<Button-1>', self.SelectCardPlayer)
+                    self.listYellow.append(button)
+                    self.Redraw(self.listYellow)
+                elif self.curButt['bg']=='blue':
+                    w["state"] = "disabled"
+                    self.listOfDisabled.append(w["text"])
+                    button = tk.Button(self.BLUE,width=wid,height=h,bd=0,image = img, bg='blue')
+                    button.image = img
+                    button.bind('<Button-1>',self.SelectCardPlayer)
+                    self.listBlue.append(button)
+                    self.Redraw(self.listBlue)
+            except:
+                print("somthing wrong")
                 
-                self.listRed.append(button)
-                print(self.listRed)
-                self.Redraw(self.listRed)
-    # except:
-        print("somthing wrong")
 
-
+    # Перерисовываем карты
     def Redraw(self, listColor):
         n=0
         for card in listColor:
@@ -286,23 +299,32 @@ class Example:
         listColor[0].grid(row=1,column=0)
         pass
     
+    # Если карту в колоде игрока нажали, она сереет или наоборот светлеет
     def SelectCardPlayer(self,e):
-        # print(123)
         w = e.widget
         self.curButt = w
 
-        # print(w.keys())
-        # print(w["bg"])
-        # print(w["state"])
         if w["state"] == "normal":
             w["state"] = "disabled"
         else:
             w["state"] = "normal"
-        # print(self.RED.grid_slaves())
         
-        
+    # Удаление карты
     def DeleteCard(self):
-        self.listRed.remove(self.curButt)
-        self.curButt.destroy()
-        self.Redraw(self.listRed)
+        if self.curButt['bg']=='red':
+            self.listRed.remove(self.curButt)
+            self.curButt.destroy()
+            self.Redraw(self.listRed)
+        elif self.curButt['bg']=='green':
+            self.listGreen.remove(self.curButt)
+            self.curButt.destroy()
+            self.Redraw(self.listGreen)
+        elif self.curButt['bg']=='yellow':
+            self.listYellow.remove(self.curButt)
+            self.curButt.destroy()
+            self.Redraw(self.listYellow)
+        elif self.curButt['bg']=='blue':
+            self.listBlue.remove(self.curButt)
+            self.curButt.destroy()
+            self.Redraw(self.listBlue)
 okno = Example()
